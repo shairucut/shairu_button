fd996bbb783d89adcb022dd12903ea88c99bb0dd
shairucut <157654186+shairucut@users.noreply.github.com>
2024-02-17 15:55:10 +0900

    Update shairu_button.js

--- shairu_button.js
+++ shairu_button.js
@@ -9,6 +9,9 @@ function reset(){
 function ring1(){  document.getElementById('a').currentTime = 0; document.getElementById('a').play(); }
 function ring2(){  document.getElementById('b').currentTime = 0; document.getElementById('b').play(); }
 function ring3(){  document.getElementById('c').currentTime = 0; document.getElementById('c').play(); }
+function ring4(){  document.getElementById('d').currentTime = 0; document.getElementById('d').play(); }
+function ring5(){  document.getElementById('e').currentTime = 0; document.getElementById('e').play(); }
+function ring6(){  document.getElementById('f').currentTime = 0; document.getElementById('f').play(); }
 /*
 avaScriptJavaScript
 (function (window, $) {
