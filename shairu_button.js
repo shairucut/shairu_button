63f1d72b9a2c646c715ad58b3b44cfaf4fec15f7
shairucut <157654186+shairucut@users.noreply.github.com>
2024-07-21 19:22:08 +0900

    Add files via upload

--- shairu_button.js
+++ shairu_button.js
@@ -13,6 +13,7 @@ function ring4(){  document.getElementById('d').currentTime = 0; document.getEle
 function ring5(){  document.getElementById('e').currentTime = 0; document.getElementById('e').play(); }
 function ring6(){  document.getElementById('f').currentTime = 0; document.getElementById('f').play(); }
 function ring7(){  document.getElementById('g').currentTime = 0; document.getElementById('g').play(); }
+function ring8(){  document.getElementById('h').currentTime = 0; document.getElementById('h').play(); }
 /*
 avaScriptJavaScript
 (function (window, $) {
